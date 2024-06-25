bbdd0d505c051c7c903ddb5ff823413729020953
Pragalbh Mandaokar <pragalbh.a.mandaokar@gmail.com>
2024-06-25 18:20:57 +0100

    companies

--- src/companies.js
+++ src/companies.js
@@ -1,9 +1,41 @@
 const companies = [
   {
-    key: 1,
+    key: 201,
     name: "Retailing",
     score: 40,
     url: "https://www.abc-mart.net/shop/",
+  },
+  {
+    key: 202,
+    name: "Food Products",
+    score: 67.3,
+    url: "https://www.adaniwilmar.com/",
+    information:
+      "Adani Wilmar Ltd is an FMCG food company offering most of the essential kitchen commodities for Indian consumers, including edible oil, wheat flour, rice, pulses, and sugar. The company also offers a diverse range of industry essentials, including oleochemicals, castor oil and its derivatives, and de-oiled cakes. The company's products are offered under a diverse range of brands across a broad price spectrum and cater to different customer groups. The company sells an entire range of packed products under the following brands: Fortune, King’s, Raag, Bullet, Fryola, Jubilee, Aadhaar, and VIVO.",
+  },
+  {
+    key: 203,
+    name: "Food Products",
+    score: 69.1,
+    url: "https://www.agthia.com/",
+    information:
+      "Adani Wilmar Ltd is an FMCG food company offering most of the essential kitchen commodities for Indian consumers, including edible oil, wheat flour, rice, pulses, and sugar. The company also offers a diverse range of industry essentials, including oleochemicals, castor oil and its derivatives, and de-oiled cakes. The company's products are offered under a diverse range of brands across a broad price spectrum and cater to different customer groups. The company sells an entire range of packed products under the following brands: Fortune, King’s, Raag, Bullet, Fryola, Jubilee, Aadhaar, and VIVO.",
+  },
+  {
+    key: 204,
+    name: "Food Products",
+    score: 52.6,
+    url: "https://www.aljouf.com.sa/language/change/en",
+    information:
+      "Adani Wilmar Ltd is an FMCG food company offering most of the essential kitchen commodities for Indian consumers, including edible oil, wheat flour, rice, pulses, and sugar. The company also offers a diverse range of industry essentials, including oleochemicals, castor oil and its derivatives, and de-oiled cakes. The company's products are offered under a diverse range of brands across a broad price spectrum and cater to different customer groups. The company sells an entire range of packed products under the following brands: Fortune, King’s, Raag, Bullet, Fryola, Jubilee, Aadhaar, and VIVO.",
+  },
+  {
+    key: 205,
+    name: "Food Products",
+    score: 52.6,
+    url: "https://www.coca-colahellenic.com/",
+    information:
+      "Coca-Cola HBC is the third-largest bottling partner in the Coca-Cola system by volume, behind Coca-Cola FEMSA and Coca-Cola Europacific Partners. In 2023, CCHBC sold 2.8 billion unit cases of beverages. For context, Coke’s largest bottler, Coke FEMSA, sold 4 billion unit cases in Latin America, and the second-largest—CCEP, serving Europe, Southeast Asia, and the South Pacific region—sold 3.3 billion unit cases. There is a long tail of Coke distributors, including brewers and independent operators with very small distribution territories.CCHBC is listed on the London Stock Exchange. Kar-Tess, a Luxembourg-based holding company, owns 23% of the equity of CCHBC, and Coca-Cola holds a further 21%. The remaining 56% is free float.",
   },
 ];
 
